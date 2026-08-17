00f9974c48c723152f2a4aefcd77e4844fbd3dee
mgismissing <gabrycraft2010@gmail.com>
2026-08-18 00:43:20 +0200

    Add `Client.signup` stub

--- src/client/api.py
+++ src/client/api.py
@@ -2,5 +2,7 @@ class Client:
     def __init__(self, username: str, password: str) -> None:
         self.username = username
         self.password = password
+    def signup(self) -> None:
+        ... # TODO client.api.signup
     def login(self) -> None:
         ... # TODO client.api.login
