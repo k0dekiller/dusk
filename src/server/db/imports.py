db02ea47fc84578a66c9fce6028cce521aa093f2
mgismissing <gabrycraft2010@gmail.com>
2026-08-18 16:45:45 +0200

    Import `typing.Any`

--- src/server/db/imports.py
+++ src/server/db/imports.py
@@ -1,6 +1,6 @@
 # pyright: reportUnusedImport=false
 from ..utils import now, hash, over
-from typing import Concatenate, Literal, Self, overload
+from typing import Concatenate, Literal, Self, Any, overload
 from collections.abc import Callable
 from functools import wraps
 from sqlite3 import Row, Cursor
