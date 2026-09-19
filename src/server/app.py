29863252821fc7683210958908d7580ab7621ed4
mgismissing <github@deadcode.it>
2026-09-19 02:08:41 +0200

    Fix imports

--- src/server/app.py
+++ src/server/app.py
@@ -2,7 +2,8 @@
 
 from collections.abc import Callable
 from sqlite3 import Row
-from typing import Any, cast
+from typing import Any, Concatenate, cast
+from functools import wraps
 from flask import Flask, jsonify
 
 from . import rest
