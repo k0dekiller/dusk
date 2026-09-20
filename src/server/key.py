87e8c8fb3a32af72b577bdfbf52a4454d6c58a91
mgismissing <github@deadcode.it>
2026-09-20 02:23:10 +0200

    Add docstrings

--- src/server/key.py
+++ src/server/key.py
@@ -1,9 +1,12 @@
+"""Provides tools that simplify key management."""
+
 import os
 
 file = f"key.bin"
 key = b""
 
 def gen() -> None:
+    """Generates a new key and saves it in `file`."""
     global key
     key = os.urandom(32)
     with open(file, "wb") as f:
