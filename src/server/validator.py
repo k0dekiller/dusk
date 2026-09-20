6e7de33336d58cb8a064bb6243df0be4ed9d2847
mgismissing <github@deadcode.it>
2026-09-20 02:45:33 +0200

    Add docstrings

--- src/server/validator.py
+++ src/server/validator.py
@@ -1,16 +1,21 @@
+"""Provides tools for regex-based string validation."""
+
 from collections.abc import Callable
 import re
 
 class NoMatchException(Exception):
-    pass
+    """Raised whenever the `Validator.check` function fails a match."""
 
 class Validator:
+    """The class that allows for regex-based string validation."""
     def __init__(self, r: str) -> None:
         self.r = r
         self.v: Callable[[str], bool] = lambda s: re.fullmatch(r, s) is not None
     def match(self, s: str) -> bool:
+        """Returns `True` if `s` matches `self.v`."""
         return self.v(s)
     def check(self, s: str) -> None:
+        """Raises `NoMatchException` if `s` doesn't match `self.v`."""
         if not self(s): raise NoMatchException(f"{repr(s)} doesn't match {self}")
     def __call__(self, s: str) -> bool:
         return self.match(s)
