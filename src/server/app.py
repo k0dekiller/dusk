7fe5dab66d935f4da10f1e05832c0cf92780c7db
mgismissing <github@deadcode.it>
2026-09-21 16:00:37 +0200

    Add docstrings

--- src/server/app.py
+++ src/server/app.py
@@ -12,15 +12,18 @@ from . import db
 from .key import key
 from . import validator as v
 
-def mkpath(path: str = "") -> Callable[..., str]:
+def mkpath(path: str = "") -> Callable[[str], str]:
+    """Returns a function that appends a subpath to `path`."""
     def p(subpath: str = "") -> str:
         return f"{path}/{subpath}"
     return p
 
 def row(row: Any) -> Row:
-    return cast(Row, row)
+    """Casts `Any` to `Row`."""
+    return row
 
 def app(db_path: str = "data.db") -> Flask:
+    """App factory; configures and returns a new `Flask` app."""
     app = Flask(__name__)
     app.config["DB_CONN"] = conn = db.Connector(db_path)
     app.config["DB_USERS"] = users = db.Users(conn)
@@ -29,8 +32,10 @@ def app(db_path: str = "data.db") -> Flask:
     app.config["SECRET_KEY"] = key
 
     class err:
+        """The namespace that contains all the error functions."""
         @staticmethod
         def invalid(param: str | None) -> response:
+            """Returns an error response specifying that its cause is in the given parameters."""
             return jsonify(rest.error(
                 rest.err.param.value.invalid,
                 desc=f"Invalid parameters",
@@ -38,6 +43,7 @@ def app(db_path: str = "data.db") -> Flask:
             )), 400
         @staticmethod
         def username_taken() -> response:
+            """Returns an error response specifying that the specified username already exists."""
             return jsonify(rest.error(
                 rest.err.param.value.not_unique,
                 desc=f"Username taken",
@@ -45,6 +51,7 @@ def app(db_path: str = "data.db") -> Flask:
             )), 400
         @staticmethod
         def wrong_login() -> response:
+            """Returns an error response specifying that the specified username and password don't match an existing user."""
             return jsonify(rest.error(
                 rest.err.param.value.invalid,
                 desc=f"Wrong username or password",
@@ -52,6 +59,7 @@ def app(db_path: str = "data.db") -> Flask:
             )), 400
         @staticmethod
         def invalid_login() -> response:
+            """Returns an error response specifying that the specified username and password are not valid."""
             return jsonify(rest.error(
                 rest.err.param.value.invalid,
                 desc=f"Invalid username or password",
@@ -59,6 +67,7 @@ def app(db_path: str = "data.db") -> Flask:
             )), 400
         @staticmethod
         def invalid_token() -> response:
+            """Returns an error response specifying that the specified token doesn't exist."""
             return jsonify(rest.error(
                 rest.err.param.value.invalid,
                 desc=f"Invalid token",
@@ -66,15 +75,18 @@ def app(db_path: str = "data.db") -> Flask:
             ))
         @staticmethod
         def invalid_invite() -> response:
+            """Returns an error response specifying that the specified invite doesn't exist."""
             return jsonify(rest.error(
                 rest.err.param.value.invalid,
                 desc=f"Invalid invite code",
                 params="invite"
             )), 400
     def success(data: rest.body | None = None) -> response:
+        """Returns a successful response with optional additional data."""
         return jsonify(rest.success(data)), 200
 
     def token[**P, R](f: Callable[Concatenate[str, P], R]) -> Callable[Concatenate[str, P], R | response]:
+        """Checks if the function `f`'s `token` is valid before running it and returning the result."""
         @wraps(f)
         def wrapper(token: str, *args: P.args, **kwargs: P.kwargs) -> R | response:
             if not tokens.valid(token=token):
@@ -83,15 +95,18 @@ def app(db_path: str = "data.db") -> Flask:
         return wrapper
 
     class Root:
+        """Defines the endpoint handlers for `/`."""
         path = mkpath()
         @classmethod
         def sub(cls, path: str = "") -> Callable[[str], str]:
+            """Returns a function that appends a subpath to this class' path plus a the subpath `path`."""
             return mkpath(cls.path(path))
 
         @app.post(path("login"))
         @rest.require("username", "password")
         @staticmethod
         def login(username: str, password: str) -> response:
+            """Handles user login."""
             if not (v.username(username) and v.password(password)):
                 return err.invalid_login()
             if not users.login(username, password):
@@ -105,6 +120,7 @@ def app(db_path: str = "data.db") -> Flask:
         @rest.require("username", "password", "invite")
         @staticmethod
         def signup(username: str, password: str, invite: str) -> response:
+            """Handles user signup."""
             # check if username and password are valid
             if not (v.username(username) and v.password(password)):
                 return err.invalid_login()
@@ -133,12 +149,14 @@ def app(db_path: str = "data.db") -> Flask:
             return success()
 
     class Users(Root):
+        """Defines the endpoint handlers for `/users`."""
         path = Root.sub("users")
         @app.post(path("<receiver>/requests"))
         @rest.require("token")
         @token
         @staticmethod
         def requests(token: str, receiver: str) -> response:
+            """Handles user friend requests."""
             return success({"token": token, "receiver": receiver})
 
     return app
