bea196fb5696b575d1be15ee11351b703ccd0f27
mgismissing <github@deadcode.it>
2026-09-22 20:38:49 +0200

    Make `_relationship` call more compact

--- src/server/app.py
+++ src/server/app.py
@@ -215,10 +215,7 @@ def app(db_path: str = "data.db") -> Flask:
         def friend(token: str, receiver: str, value: bool) -> response:
             """Handles user friend settings."""
             return Users._relationship(
-                token,
-                receiver,
-                "friend",
-                value,
+                token, receiver, "friend", value,
                 "Receiver is already a friend"
             )
 
