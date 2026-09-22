d37a04beaf2027f6cfa9295715147df9260f7287
mgismissing <github@deadcode.it>
2026-09-22 17:02:05 +0200

    Add CORS for test clients

--- tests/server/app.py
+++ tests/server/app.py
@@ -1,4 +1,10 @@
 from src.server.app import app
+from flask_cors import CORS
 
 server = app("test_api.db")
+CORS(server, resources={
+    r"/*": {
+        "origins": ["http://localhost:8080"]
+    }
+})
 server.run("localhost", 8080, debug=True)
